f245b6ab35baef4fcd757d80992a5c1cfea3bc0d
Amol Mahale <mahale.amol777@gmail.com>
2025-10-29 04:08:26 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,8 +42,10 @@ resource "azurerm_kubernetes_cluster" "aks" {
     type = "SystemAssigned"
   }
 
-  role_based_access_control {
-    enabled = true
+  azure_active_directory_role_based_access_control {
+  managed = true
+  }
+
   }
 
   network_profile {
