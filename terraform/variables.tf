6d9773491ade301d858c76f19c37e5265c3f9091
Amol Mahale <mahale.amol777@gmail.com>
2025-10-29 03:03:17 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,36 @@
+variable "prefix" {
+  type    = string
+  default = "demo"
+}
+
+variable "location" {
+  type    = string
+  default = "East US"
+}
+
+variable "resource_group_name" {
+  type    = string
+  default = "${var.prefix}-rg"
+}
+
+variable "aks_node_count" {
+  type    = number
+  default = 2
+}
+
+variable "aks_node_size" {
+  type    = string
+  default = "Standard_DS2_v2"
+}
+
+variable "acr_name" {
+  type    = string
+  default = "${var.prefix}acr"
+}
+
+# Optional: storage account for tfstate backend (if you decide to use remote backend)
+variable "tf_backend_storage_account" {
+  type    = string
+  default = ""
+}
 
